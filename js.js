58634e9d09b6b43f3caf0f24db21a6aeb2faa2aa
keil007 <ejmont.m@gmail.com>
2020-11-18 01:59:05 +0100

    just small insignifcant changes

--- js.js
+++ js.js
@@ -9,3 +9,14 @@ $(document).ready(function() {
     }
   });
 });
+/*
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajax_info.json", true);
+  xhttp.send();}
